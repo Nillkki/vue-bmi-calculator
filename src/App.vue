9afdd7a763481c988cb2360f1705da1d48ab854d
Jens Holmberg <jens.holmberg@protonmail.com>
2020-06-24 21:15:21 +0300

    Add simple calculator

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>BMI Calculator</h1>
+    <label for="weight">Weight</label>
+    <input type="number" id="weight" v-model.lazy="weight" :placeholder="'Enter weight in kg'">
+    <label for="height">Height</label>
+    <input type="number" id="height" v-model.lazy="height" :placeholder="'Enter height in cm'">
+    <button v-on:click="calculateBmi" :disabled='!isFilled'>Calculate</button>
+    <p> Your BMI is {{ bmi }} </p>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+@Component
+export default class App extends Vue {
+  weight = 0
+
+  height = 0
+
+  bmi = 0
+
+  calculateBmi() {
+    this.bmi = (this.weight / ((this.height / 100) ** 2)).toFixed(2);
+  }
+
+  // Declared as computed property getter
+  get isFilled() {
+    return this.weight && this.height;
+  }
+}
 </script>
 
 <style lang="scss">
